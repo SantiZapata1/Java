565dd7b351bd8fa890bbf6876617f9f6be48e511
Santiago Zapata <santiago@MacBook-Pro-de-Santiago.local>
2023-11-09 17:53:19 -0300

    parcial 2 parte 2

--- src/Parcial2Lab/Hospital.java
+++ src/Parcial2Lab/Hospital.java
@@ -1,10 +1,11 @@
 package Parcial2Lab;
 
 import java.sql.*;
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Hospital {
-    
+
 
 
     //metodo para agregar paciente a la lista
@@ -13,7 +14,8 @@ public class Hospital {
 
         try{
             // Consulta SQL para insertar un nuevo estudiante en la base de datos.
-            // Utiliza signos de interrogaci�n como marcadores de posici�n para los valores.
+            // Utiliza signos de interrogacion como marcadores de posicion para los valores.
+
             String consulta = "INSERT INTO pacientes (nombre, edad, historial_medico, fecha_ingreso, doctor) VALUES (?, ?, ?, ?, ?)";
 
             // Crea un PreparedStatement para ejecutar la consulta SQL con valores reales.
@@ -23,16 +25,19 @@ public class Hospital {
             preparedStatement.setString(2, paciente.getEdad()); // Asigna el valor de legajo al tercer marcador de posici�n
             preparedStatement.setString(3, paciente.getHistorialMedico()); // Asigna el valor de dni al cuarto marcador de posici�n
             preparedStatement.setString(4, "2000-10-10"); // Asigna el valor de dni al cuarto marcador de posici�n
-            preparedStatement.setString(5, "0"); // Asigna el valor de dni al cuarto marcador de posici�n
+            preparedStatement.setString(5, "0"); // Asigna el valor de dni al cuarto marcador de posicion
+
 
             // Ejecuta la consulta y obtiene el n�mero de filas afectadas.
             int filasAfectadas = preparedStatement.executeUpdate();
 
             // Verifica si la inserci�n fue exitosa y muestra un mensaje apropiado.
             if (filasAfectadas > 0) {
-                System.out.println("Estudiante agregado exitosamente.");
+                System.out.println("Paciente agregado exitosamente.");
+                mostrarListaPacientes(conexion);
+
             } else {
-                System.out.println("No se pudo agregar el estudiante.");
+                System.out.println("No se pudo agregar el paciente.");
             }
 
             // Cierra el PreparedStatement para liberar recursos.
@@ -45,15 +50,61 @@ public class Hospital {
             e.getCause();
         }
 
-
-
-
     }
 
     //metodo para eliminar paciente a la lista
-    public static void eliminarPaciente(Paciente paciente) {
+    public static void eliminarPaciente(int IdEliminar, Connection conexion) {
 
-        System.out.println("eliminar paciente");
+        try{
+
+            // Verificar si el estudiante con el ID especificado existe en la base de datos.
+            String consultaExistencia = "SELECT * FROM pacientes WHERE id = ?";
+
+            PreparedStatement preparedStatementExistencia = conexion.prepareStatement(consultaExistencia);
+
+            preparedStatementExistencia.setInt(1, IdEliminar); // Establece el valor del marcador de posici�n.
+
+            ResultSet resultadoExistencia = preparedStatementExistencia.executeQuery();
+
+            // Si no se encuentra ning�n estudiante con el ID proporcionado, muestra un mensaje y sale de la funci�n.
+            if (!resultadoExistencia.next()) {
+                System.out.println("El paciente no existe.");
+                preparedStatementExistencia.close();
+                return;
+            }else {
+                // Consulta SQL para eliminar el estudiante de la base de datos.
+                String consulta = "DELETE FROM pacientes WHERE id = ?";
+
+                PreparedStatement preparedStatement = conexion.prepareStatement(consulta);
+
+                preparedStatement.setInt(1, IdEliminar); // Establece el valor del marcador de posici�n.
+
+                // Ejecuta la consulta SQL y obtiene el n�mero de filas afectadas.
+                int filasAfectadas = preparedStatement.executeUpdate();
+
+                // Verifica si la eliminaci�n fue exitosa y muestra un mensaje apropiado.
+                if (filasAfectadas > 0) {
+                    System.out.println("paciente eliminado exitosamente.");
+                    mostrarListaPacientes(conexion);
+
+                } else {
+                    System.out.println("No se pudo eliminar el paciente.");
+                }
+
+                // Cierra el PreparedStatement para liberar recursos.
+                preparedStatement.close();
+            }
+
+
+
+        }catch (SQLException e) {
+            System.out.println("Error: No se pudo conectar a la base de datos");
+            e.printStackTrace();
+            e.getMessage();
+            e.getCause();
+        }
+
+
     }
 
     //metodo para mostrar la lista de pacientes
@@ -82,6 +133,7 @@ public class Hospital {
 
             // Itera a trav�s de los resultados y muestra los datos de cada estudiante en forma de tabla.
             while (resultado.next()) {
+
                 int id = resultado.getInt("id");
                 String nombre = resultado.getString("nombre");
                 int edad = resultado.getInt("edad");
@@ -110,14 +162,113 @@ public class Hospital {
     }
 
     //metodo para asignar un doctor a cada paciente
-    public static void asignarDoctorCabecera(Paciente paciente, Doctor doctor){
+    public static void asignarDoctorCabecera(int idDoctor, int idPaciente, Connection conexion){
 
-        System.out.println("asigar doctor a paciente");
+        try{
+            // Verificar si el estudiante con el ID especificado existe en la base de datos.
+            String consultaExistencia = "SELECT * FROM pacientes WHERE id = ?";
+            PreparedStatement preparedStatementExistencia = conexion.prepareStatement(consultaExistencia);
+            preparedStatementExistencia.setInt(1, idPaciente); // Establece el valor del marcador de posici�n.
+            ResultSet resultadoExistencia = preparedStatementExistencia.executeQuery();
+
+            // Si no se encuentra ning�n estudiante con el ID proporcionado, muestra un mensaje y sale de la funci�n.
+            if (!resultadoExistencia.next()) {
+                System.out.println("El paciente no existe.");
+                preparedStatementExistencia.close();
+            }
+            else{
+
+                // Consulta SQL para actualizar los datos del estudiante en la base de datos.
+                String consulta = "UPDATE pacientes SET doctor = ? WHERE id = ?";
+
+                // Crea un PreparedStatement para ejecutar la consulta SQL con valores reales.
+                PreparedStatement preparedStatement = conexion.prepareStatement(consulta);
+                preparedStatement.setInt(1, idDoctor);
+                preparedStatement.setInt(2, idPaciente);
+
+                // Ejecuta la consulta SQL y obtiene el n�mero de filas afectadas.
+                int filasAfectadas = preparedStatement.executeUpdate();
+
+                // Verifica si la edici�n fue exitosa y muestra un mensaje apropiado.
+                if (filasAfectadas > 0) {
+                    System.out.println("Estudiante editado exitosamente.");
+                    mostrarListaPacientes(conexion);
+
+                } else {
+                    System.out.println("No se pudo editar el estudiante.");
+                }
+
+                // Cierra el PreparedStatement para liberar recursos.
+                preparedStatement.close();
+
+            }
+
+
+        }catch (SQLException e) {
+            System.out.println("Error: No se pudo conectar a la base de datos");
+            e.printStackTrace();
+            e.getMessage();
+            e.getCause();
+        }
+
+
     }
 
     //metodo  para mostrar pacientes entre fechas
-    public static void mostrarPacientesEntreFechas(String fechaInicio, String fechaFin) {
-        System.out.println("lista de pacientes entre fechas:");
+    public static void mostrarPacientesEntreFechas(Connection conexion, LocalDate fechaInicio, LocalDate fechaFin) {
+
+
+        try{
+
+            Statement statement = conexion.createStatement();
+
+            // Define la consulta SQL para obtener todos los estudiantes.
+            String consulta = "SELECT * FROM pacientes ";
+
+            // Ejecuta la consulta SQL y almacena los resultados en un ResultSet.
+            ResultSet resultado = statement.executeQuery(consulta);
+
+            // Imprime una cabecera de columnas para los datos de los estudiantes.
+            System.out.printf("%-4s%-20s%-6s%-20s%-20s%-10s\n", "ID", "Nombre", "Edad", "Historial Médico", "Fecha de ingreso", "Doctor");
+
+            // Itera a trav�s de los resultados y muestra los datos de cada estudiante en forma de tabla.
+            while (resultado.next()) {
+
+                int id = resultado.getInt("id");
+                String nombre = resultado.getString("nombre");
+                int edad = resultado.getInt("edad");
+                String historialMedico = resultado.getString("historial_medico");
+                String fechaIngreso = resultado.getString("fecha_ingreso");
+                String doctor = resultado.getString("doctor");
+
+                LocalDate FechaIngreso = LocalDate.parse(fechaIngreso);
+
+
+                if (FechaIngreso.isAfter(fechaInicio) && FechaIngreso.isBefore(fechaFin)){
+                    // Imprime los datos del estudiante con tabulaciones para formatear como una tabla.
+                    System.out.printf("%-4d%-20s%-6d%-20s%-20s%-10s\n", id, nombre, edad, historialMedico, fechaIngreso, doctor);
+                }
+
+
+
+
+            }
+
+            // Cierra el ResultSet y la declaraci�n para liberar recursos.
+            resultado.close();
+            statement.close();
+
+
+        }catch (SQLException e) {
+            System.out.println("Error: No se pudo conectar a la BD en metodo mostrar Pacientes");
+            e.printStackTrace();
+            e.getMessage();
+            e.getCause();
+        }
+
+
+
+
     }
 
 
@@ -169,11 +320,11 @@ public class Hospital {
                         break;
                     case 2:
 
-                        System.out.print("ingrese EDAD del nuevo paciente:");
-                        String edadNuevo= scanner.next();
-
                         System.out.print("ingrese NOMBRE del nuevo paciente:");
                         String nombreNuevo = scanner.next();
+
+                        System.out.print("ingrese EDAD del nuevo paciente:");
+                        String edadNuevo= scanner.next();
 
                         System.out.print("ingrese HISTORIAL del nuevo paciente:");
                         String historialNuevo = scanner.next();
@@ -185,17 +336,35 @@ public class Hospital {
                         break;
                     case 3:
 
-//                        eliminarPaciente();
+                        System.out.print("ingrese ID del paciente a eliminar:");
+                        int IdEliminar = scanner.nextInt();
+
+                        eliminarPaciente(IdEliminar, conexion);
 
                         break;
                     case 4:
 
-//                        asignarDoctorCabecera();
+                        System.out.print("ingrese ID del doctor que quiere asignar:");
+                        int idDoctor = scanner.nextInt();
+
+                        System.out.print("al paciente con ID:");
+                        int IdPaciente = scanner.nextInt();
+
+                        asignarDoctorCabecera(idDoctor, IdPaciente, conexion);
 
                         break;
                     case 5:
 
-//                        mostrarPacientesEntreFechas();
+                        System.out.print("Ingrese una fecha de inicio(formato YYYY-MM-DD): ");
+                        String fecha1 = scanner.next();
+
+                        System.out.print("Ingrese una fecha limite (formato YYYY-MM-DD): ");
+                        String fecha2 = scanner.next();
+
+                        LocalDate fechaInicio = LocalDate.parse(fecha1);
+                        LocalDate fechaFin = LocalDate.parse(fecha2);
+
+                        mostrarPacientesEntreFechas(conexion, fechaInicio, fechaFin);
 
                         break;
 
@@ -239,7 +408,7 @@ abstract class Persona {
     //atributos
     protected String nombre;
     protected String edad;
-    protected String id;
+    protected int id;
 
     // Constructor y métodos necesarios
     Persona( String nombre, String edad){
@@ -265,7 +434,7 @@ abstract class Persona {
         this.edad = edad;
     }
 
-    public String getId() {
+    public int getId() {
         return id;
     }
 
@@ -278,7 +447,7 @@ class Paciente extends Persona {
     private String historialMedico;
 
     // Constructor y métodos necesarios
-    Paciente( String nombre, String edad,  String historialMedico){
+    Paciente(String nombre, String edad,  String historialMedico){
 
         super(nombre,edad);
         this.historialMedico=historialMedico;
